a91c7aebd70ad22b173f16309081123e34b855b3
activecoder <yash0312@hotmail.com>
2018-09-28 22:17:52 -0400

    multidimensional

---
+++ workspace/MultiDimensional/src/app.java
@@ -0,0 +1,18 @@
+
+public class app {
+	public static void main(String[] args) {
+
+		int[][] grid = {
+
+		{ 1, 2, 3, 4 }, 
+		{ 4, 23, 56, 9 }, 
+		{ 34, 5, 1, 7 } };
+
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				System.out.print(grid[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ workspace/words in order/src/app.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class app {
+	private static Scanner input;
+
+	public static void main(String[] args) {
+
+		input = new Scanner(System.in);
+		
+		System.out.println("Enter in a string:");
+		String value = input.nextLine();
+		
+		for(int i = 0;i < value.length(); i++) {
+		 	
+		}
+		
+	}
+}
