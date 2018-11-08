e63b9d072f5c650c3db7d598fdf472101b8fa189
activecoder <yash0312@hotmail.com>
2018-11-08 12:21:35 -0500

    update

--- workspace/Garage/src/CarGarage.java
+++ workspace/Garage/src/CarGarage.java
@@ -1,39 +1,45 @@
+import java.util.ArrayList;
 
 public class CarGarage {
-
+	ArrayList<Car> myArrCar = new ArrayList<Car>();
 	private int i = 0;
-	
+
 	void Enter(Car car) {
+		myArrCar.add(car);
 		i++;
-		System.out.println("Car entered garage: Make:" + car.GetMake() + " Model:" + car.GetModel());
 	}
 
 	void Exit(Car car) {
+		myArrCar.remove(car);
 		i--;
-		System.out.println("Car exited garage: Make:" + car.GetMake() + " Model:" + car.GetModel());
 	}
 
 	int CountCar() {
 		return i;
 	}
-	
+
+	void carMakeModel() {
+		for (int i = 0; i < myArrCar.size(); i++) {
+			Car c = myArrCar.get(i);
+			String x = c.GetModel();
+			String y = c.GetMake();
+			System.out.println(y + " " + x);
+		}
+	}
+
 	public static void main(String[] args) {
 
 		HondaOdyssey hondaOdyssey = new HondaOdyssey(false, true, "Blue");
 		NissanRogue nissanCar = new NissanRogue(true, false, "silver");
-		
+
 		CarGarage garage = new CarGarage();
-		
-		
-		
+
 		garage.Enter(hondaOdyssey);
 		garage.Enter(nissanCar);
+		garage.Exit(hondaOdyssey);
 		garage.Enter(nissanCar);
-		garage.Enter(nissanCar);
-		garage.Exit(nissanCar);
-		Car car = new Car(100,10);
-		garage.Enter(car);
-	
-		System.out.println(garage.CountCar());
+		garage.Enter(hondaOdyssey);
+
+		garage.carMakeModel();
 	}
 }
