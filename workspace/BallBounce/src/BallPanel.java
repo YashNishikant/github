94764d80f2baf0579526b8f2a55a9dd7119b7872
activecoder <yash0312@hotmail.com>
2019-02-25 22:01:08 -0500

    Paddle moves

--- workspace/BallBounce/src/BallPanel.java
+++ workspace/BallBounce/src/BallPanel.java
@@ -1,35 +1,129 @@
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Graphics;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class BallPanel extends JPanel {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.Timer;
 
-	boolean up = false;
+public class BallPanel extends JPanel implements ActionListener, KeyListener {
 
-	int a = 50;
-	int b = 100;
+	Timer time = new Timer(5, this);
+	int x = 210, speedX = 0;
+
+	public BallPanel() {
+		time.start();
+		addKeyListener(this);
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(false);
+	}
+
+	boolean down = true;
+	boolean right = true;
+	int a = 0;
+	int b = 300;
 
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
+		g.setColor(Color.gray);
+		g.fillRect(x, 540, 200, 300);
+		
 		g.setColor(Color.red);
-		
-		if (up == false) {
-			g.fillOval(a--, b++, 50, 50);
+
+		// down right
+		if (a == 0) {
+			down = true;
+			right = true;
 		}
 
-		else {
-			g.fillOval(a--, b++, 50, 50);
+		// up right
+		if (b == 510) {
+			down = false;
+			right = true;
 		}
 
-		if (a == 510 || b == 510) {
-			up = true;
+		// up left
+		if (a == 510) {
+			down = false;
+			right = false;
 		}
-		
-		if (a == 0 || b == 0) {
-			up = false;
+
+		// down left
+		if (b == 0) {
+			down = true;
+			right = false;
 		}
+
+		if (down == true) {
+			b++;
+		} else {
+			b--;
+		}
+
+		if (right == true) {
+			a++;
+		} else {
+			a--;
+		}
+		g.fillOval(a, b, 50, 50);
+ 
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		x = x + speedX;
+		repaint();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int i = e.getKeyCode();
+
+		if (i == KeyEvent.VK_D) {
+
+			speedX = 1;
+
+		}
+
+		if (i == KeyEvent.VK_A) {
+
+			speedX = -1;
+
+		}
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {}
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+	
+	speedX = 0;
 		
 	}
+
+	
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		
+		Container contentpane = frame.getContentPane();
+		BallPanel BPanel = new BallPanel();
+		
+		Dimension preferredSize = new Dimension();
+		preferredSize.setSize(600, 600);
+		
+		//newPanel.setSize(preferredSize);
+		frame.setSize(preferredSize);
+		contentpane.add(BPanel);
+	
+		frame.setVisible(true);
+	}
+	
 }
